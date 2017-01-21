94b11614491cd7dbfe3662fcc599bb10045bd304
Bob Jacobs <jacobsrj@hotmail.com>
2017-01-21 13:49:01 -0500

    adding client application

---
+++ Alphabet.Client/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Client;
+using System.Fabric;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+
+namespace Alphabet.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri alphabetServiceUri = new Uri(@"fabric:/AlphabetPartitions/Processing");
+            string connectionEndpoint1 = ConfigurationManager.AppSettings["ClusterConnection1"];
+            string connectionEndpoint2 = ConfigurationManager.AppSettings["ClusterConnection2"];
+
+            HttpClient httpClient = new HttpClient();
+
+            //ServicePartitionResolver servicePartitionResolver = ServicePartitionResolver.GetDefault();
+            ServicePartitionResolver servicePartitionResolver = new ServicePartitionResolver(connectionEndpoint1, connectionEndpoint2);
+
+            string lastname = string.Empty;
+            string output = string.Empty;
+
+            CancellationToken cancelRequest = new CancellationToken();
+
+            while (string.Compare(lastname, "quit") != 0)
+            {
+                Console.WriteLine("Please enter a last name");
+                lastname = Console.ReadLine();
+
+                char firstLetterOfLastName = lastname.First();
+                ServicePartitionKey partitionKey = new ServicePartitionKey(Char.ToUpper(firstLetterOfLastName) - 'A');
+
+                // This contacts the Service Fabric Naming Services to get the addresses of the replicas of the processing service 
+                // for the partition with the partition key generated above. 
+                // Note that this gets the most current addresses of the partition's replicas,
+                // however it is possible that the replicas have moved between the time this call is made and the time that the address is actually used
+                // a few lines below.
+                // For a complete solution, a retry mechanism is required.
+                // For more information, see http://aka.ms/servicefabricservicecommunication
+                ResolvedServicePartition partition = servicePartitionResolver.ResolveAsync(alphabetServiceUri, partitionKey, cancelRequest).Result;
+                ResolvedServiceEndpoint ep = partition.GetEndpoint();
+
+                JObject addresses = JObject.Parse(ep.Address);
+                string primaryReplicaAddress = (string)addresses["Endpoints"].First();
+
+                UriBuilder primaryReplicaUriBuilder = new UriBuilder(primaryReplicaAddress);
+                primaryReplicaUriBuilder.Query = "lastname=" + lastname;
+
+                string result = httpClient.GetStringAsync(primaryReplicaUriBuilder.Uri).Result;
+                output = String.Format(
+                    "Result: {0}. <p>Partition key: '{1}' generated from the first letter '{2}' of input value '{3}'. <br>Processing service partition ID: {4}. <br>Processing service replica address: {5}",
+                    result,
+                    partitionKey,
+                    firstLetterOfLastName,
+                    lastname,
+                    partition.Info.Id,
+                    primaryReplicaAddress);
+
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# remoteClusterSample
-Sample of Service Fabric client connecting to remote cluster
+## Run this sample
+
+Alphabet partitions is an intro to partitioning stateful services in Service Fabric. It uses letters of the alphabet as partition keys into a stateful service with 26 partitions - one for each letter of the alphabet.
+
+To run this services:
+
+1. Open the .sln solution file in Visual Studio 2015
+2. Press F5 to run
+
+You can access the application in a web browser by going to:
+
+**http://localhost:8081/alphabetpartitions?lastname=test**
+
+Try different values for lastname to see data get sent to different partitions.
+
+## Next Steps
+
+1. Deploy the Service Fabric application to a remote cluster
+2. Update the client App.config to contain to two remote connection strings to the cluster (you should find these from the cluster itself)
+3. Execute the client executable and note the partition id
+
+
+- [Read more about partitioning](https://azure.microsoft.com/en-us/documentation/articles/service-fabric-concepts-partitioning)
+ 
